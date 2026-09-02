9eac1c0c8796d0bdc462e0708221bf8dcd872826
dostia052-wq <dostia052@gmail.com>
2026-09-02 14:34:03 +0330

    Fix bot implementation for ptb v20

--- air.py
+++ air.py
@@ -1,12 +1,14 @@
 import os
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = os.getenv("OWNER_ID")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات ایردراپ با موفقیت روشن شد و روی رندر کار می‌کند.")
+    await update.message.reply_text("سلام! ربات ایردراپ با موفقیت روشن شد.")
 
 def main():
     if not BOT_TOKEN:
@@ -14,7 +16,6 @@ def main():
         return
 
     application = ApplicationBuilder().token(BOT_TOKEN).build()
-    
     application.add_handler(CommandHandler("start", start))
     
     print("ربات در حال اجراست...")
